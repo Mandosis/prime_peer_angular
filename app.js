c86d0d7491a2170213ac268b49dc20754b445003
Chris Rabuse <rab233@live.com>
2016-04-07 10:13:45 -0500

    Added button to remove completed tasks

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ var app = angular.module('todo', []);
 
 app.controller('MainController', function($scope) {
   $scope.items = [];
+  $scope.showError = false;
+
   $scope.addItem = function(item) {
-    console.log('New item:', item);
-    $scope.items.push(item);
+
+    if($scope.item === '') {
+      $scope.showError = true;
+    } else {
+      $scope.showError = false;
+      console.log('New item:', item);
+      $scope.items.push({name: item, complete: false});
+      $scope.tempTask = '';
+    }
+  };
+
+  $scope.removeChecked = function() {
+    $scope.items = $scope.items.filter(function(task) {
+      return task.complete == false;
+    });
   };
 });
